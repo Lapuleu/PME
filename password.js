149b1a338f47b606fc2075058cbfa47ba2b3fcbf
Lapuleu <saibi.kerim@gmail.com>
2025-09-20 17:17:31 -0400

    server node

--- password.js
+++ password.js
@@ -6,19 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
         const formData = new FormData(myForm);
         const newPlabel = formData.get("pLabel");
         const newPassword = formData.get("password");
-        savedPasswordsList.innerHTML += `<li>${newPlabel} <button class="delete-button">Delete</button></li>`;  
         // Call Wolfram Cloud API for SHA-256 hash
-        for(let i = 0; i < 5; i++){
-            fetch('https://www.wolframcloud.com/obj/silversharkan/sha256hash' + encodeURIComponent(newPassword))
+        fetch('http://localhost:3000/sha256hash?input=' + encodeURIComponent(newPassword))
             .then(response => response.text())
             .then(hash => {
+                savedPasswordsList.innerHTML += `<li>${newPlabel}: ${hash} <button class="delete-button">Delete</button></li>`;
                 console.log(newPlabel + ": " + hash);
-                newPassword.value = hash;
             })
             .catch(error => {
                 console.error('Error calling Wolfram API:', error);
             });
-        }
     });
     savedPasswordsList.addEventListener('click', function(event) {
         if (event.target.classList.contains('delete-button')) {
